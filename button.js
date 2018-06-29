a0bd0a8610d12c0963ad4ded1d90e31c48bf1e4e
elynnchen <44544738@qq.com>
2018-06-29 18:15:33 +0800

    添加按钮基础文件

---
+++ button.js
@@ -0,0 +1,5 @@
+Vue.component('g-button',{
+    template:`
+    <button class="button">按钮</button>
+    `
+})
---
+++ src/button.vue
@@ -0,0 +1,12 @@
+<template>
+    <button class="button">按钮</button>
+</template>
+
+<script>
+    export default { }
+
+</script>
+
+<style>
+    .button{padding:5px 10px; color:red}
+</style>
