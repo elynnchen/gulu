09d540c3cf6c8944125209005ed9ed827211ca02
elynnchen <44544738@qq.com>
2018-07-04 17:36:57 +0800

    buttongroup检查格试问题

--- src/button-group.vue
+++ src/button-group.vue
@@ -3,6 +3,17 @@
 </template>
 
 <script>
+    export default{
+        mounted(){
+            for(let node of this.$el.children){
+                if(node.nodeName.toLocaleLowerCase() !=='button'){
+                    console.warn('g-button-group的子元素应是Button')
+                }
+            }
+
+
+        }/*当button-group组件出现在在页面时会触发一个函数,检测组件不允许外包Dome结构*/
+    }
 
 </script>
 
