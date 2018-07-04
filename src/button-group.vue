62082518d53b519857898ffb5149bc89631dc0a8
elynnchen <44544738@qq.com>
2018-07-04 17:25:43 +0800

    添加按钮组合

---
+++ src/button-group.vue
@@ -0,0 +1,15 @@
+<template>
+<div class="g-button-group"><slot></slot></div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+.g-button-group{
+    vertical-align: middle;
+}
+.g-button-group .button{border-radius:0; float:left; margin-left:-1px;}
+
+</style>
